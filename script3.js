413cf379f7c4581c918bcf3edb0d91cb329eb4d1
solsikke74 <117269718+solsikke74@users.noreply.github.com>
2026-08-18 10:05:50 +0200

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,8 +1,8 @@
 const steam = ["Counter-Strike 2 ","Minecraft ", "Grand Theft Auto V ", "Fortnite ", "EA Sports FC 25 " ];
 
-let text = "";
+let text = ""; //Vi lager en variabel som samler alle spillnavn i en tekststreng. Vi starter med en tom streng. 
 
-for (let x of steam){
+for (let x of steam){ //Går gjennom alle elementene i arrayet "steam", et element om gangen. For hvert element, lagres det i variabelen "x".
     text += x + "<br>";
 }
 
