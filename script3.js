af4a6f8f9138ca9fd2b591dea7384ea0d0a2ddd4
solsikke74 <117269718+solsikke74@users.noreply.github.com>
2026-08-20 14:49:56 +0200

    20262008_03

--- script3.js
+++ script3.js
@@ -8,18 +8,19 @@ for (let x of steam){ //Går gjennom alle elementene i arrayet "steam", et eleme
 
 document.getElementById("Steam").innerHTML = text;
 
-const gamingTall = [1, 4, 8, 1337, 404, 420, 117, 9000];
+const GamingTall = [1, 4, 8, 1337, 404, 420, 117, 9000];
 
 let sum = ""; //Vi lager en variabel som samler alle tallene i arrayet. Vi starter med 0.
-for (let i = 0; i < gamingTall.length; i++) { //Går gjennom alle elementene i arrayet "gamingTall", et element om gangen. For hvert element, lagres det i variabelen "i".
-    let x = gamingTall[i]; //Vi henter ut elementet i arrayet som har indeksen "i" og lagrer det i variabelen "x".
+for (let i = 0; i < GamingTall.length; i++) { //Går gjennom alle elementene i arrayet "gamingTall", et element om gangen. For hvert element, lagres det i variabelen "i".
+    let x = GamingTall[i]; //Vi henter ut elementet i arrayet som har indeksen "i" og lagrer det i variabelen "x".
     sum += x + ", "; //Vi legger til elementet i variabelen "sum" og legger til et komma og et mellomrom etter hvert element.
 }
 document.getElementById("GamingTall").innerHTML = sum;
 
-
+let resultat = ""; //Vi lager en variabel som samler alle tallene i arrayet som er over 10. Vi starter med en tom streng.
 for (let x of GamingTall) { //Går gjennom alle elementene i arrayet "gamingTall", et element om gangen. For hvert element, lagres det i variabelen "x".
     if (x > 10) {
-document.getElementById(x).innerText = x;
+        resultat += x + ", ";
     }
 }
+document.getElementById("TallOver10").innerText = resultat;
